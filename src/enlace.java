cf61442ad49502fb41132516de57c3b09825148c
LuisCarlosR2002 <rosero21318@gmail.com>
2021-07-08 20:02:25 -0500

    Commit inicial

---
+++ src/enlace.java
@@ -0,0 +1,7 @@
+public class enlace {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int r=8, s=5,suma=r+s;
+        System.out.println(suma);
+    }
+}
