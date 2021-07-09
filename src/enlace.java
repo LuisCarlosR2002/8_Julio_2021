f02095921bcdc74d3aa34b994de41b16b46fa7e1
LuisCarlosR2002 <rosero21318@gmail.com>
2021-07-08 21:47:11 -0500

    Commit cuaternario

--- src/enlace.java
+++ src/enlace.java
@@ -3,6 +3,6 @@ public class enlace {
         System.out.println("Hello, World!");
         int r=8, s=5,suma=r+s;
         System.out.println("La suma es "+r+"+"+s+"="+suma);
-        System.out.println("La division es "+r+"/"+s+"="+r/5);
+        System.out.println("La division es "+r+"/"+s+"="+r/s);
     }
 }
