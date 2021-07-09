7b1810decac95b7b3fe5ad20adf718cf6dd47b95
LuisCarlosR2002 <rosero21318@gmail.com>
2021-07-08 20:12:01 -0500

    Commit secundario

--- src/enlace.java
+++ src/enlace.java
@@ -2,6 +2,6 @@ public class enlace {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int r=8, s=5,suma=r+s;
-        System.out.println(suma);
+        System.out.println("La suma es"+r+"+"+s+"="+suma);
     }
 }
